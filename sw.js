546271925814a77b3a633ec76677cf68869e6401
caueccipriano <caueccipriano@gmail.com>
2026-09-17 17:41:34 -0300

    chore: refresh V60 full-screen cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'v60-v3';
+const CACHE = 'v60-v4';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
