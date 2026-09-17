750c1d17ebd8f38a1b28ff88121686eb2664a974
caueccipriano <caueccipriano@gmail.com>
2026-09-17 18:07:45 -0300

    chore: bump cache for animated exercise guides

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'v60-v5';
-const ASSETS = ['./','./index.html','./styles.css','./exercise-guides.css','./app.js','./exercise-guides.js','./manifest.json','./assets/icon.svg'];
+const CACHE = 'v60-v6';
+const ASSETS = ['./','./index.html','./styles.css','./exercise-guides.css','./exercise-guides-v2.css','./app.js','./exercise-guides.js','./exercise-guides-v2.js','./manifest.json','./assets/icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
