c1caabc1aef761022afae0385e1bfc5b32c5cec1
caueccipriano <caueccipriano@gmail.com>
2026-09-18 11:59:48 -0300

    fix: refresh PWA cache for workout list v38

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v37';
+const CACHE = 'traco-v38';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
