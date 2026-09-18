465fe981b9f183724bf3fe4e5fcbcafc320ade95
caueccipriano <caueccipriano@gmail.com>
2026-09-18 06:08:24 -0300

    chore: bump offline cache for exercise ordering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v14';
+const CACHE = 'traco-v15';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
