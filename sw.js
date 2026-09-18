b830acb252b3a595fd5ea8b2dd2b9db16653fbc2
caueccipriano <caueccipriano@gmail.com>
2026-09-18 07:26:04 -0300

    chore: bump offline cache for Traço 2.3.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v20';
+const CACHE = 'traco-v21';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
