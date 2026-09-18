fadcf5d9bec0a6f76be6e8faedd34d855c2435b3
caueccipriano <caueccipriano@gmail.com>
2026-09-18 06:56:49 -0300

    chore: bump Traço brand to 2.3.5

--- traco-brand.js
+++ traco-brand.js
@@ -1,4 +1,4 @@
-window.TRACO_BRAND={name:'Traço',version:'2.3.4',legacyDataPrefix:'v60_'};
+window.TRACO_BRAND={name:'Traço',version:'2.3.5',legacyDataPrefix:'v60_'};
 const TRACO_THEME_KEY='traco_theme';
 
 function tracoTheme(){
@@ -36,7 +36,7 @@ exportData=function(){
   }
   const data={
     brand:'Traço',
-    version:'2.3.4',
+    version:'2.3.5',
     sessions:sessions(),
     body:body(),
     settings:settings(),
@@ -91,7 +91,7 @@ renderSettings=function(){
   tracoBaseRenderSettings();
   const intro=document.querySelector('.settings-intro');
   if(intro&&!document.querySelector('.traco-about-card')){
-    intro.insertAdjacentHTML('afterend','<section class="traco-about-card"><img src="./assets/traco-icon-180.png" alt=""><div><span>seu app</span><b>Traço</b><small>treino · evolução · constância · versão 2.3.4</small></div></section>');
+    intro.insertAdjacentHTML('afterend','<section class="traco-about-card"><img src="./assets/traco-icon-180.png" alt=""><div><span>seu app</span><b>Traço</b><small>treino · evolução · constância · versão 2.3.5</small></div></section>');
   }
   const about=document.querySelector('.traco-about-card');
   if(about&&!document.querySelector('.traco-theme-card')){
