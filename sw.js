6236f68ee347af91614207964bc715f0cbb11771
caueccipriano <caueccipriano@gmail.com>
2026-09-18 12:45:07 -0300

    release: invalidate stale workout caches

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v42';
+const CACHE = 'traco-v43';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
