0d0c24ee283131eb0dc5234e1dd524f7decd1ade
caueccipriano <caueccipriano@gmail.com>
2026-09-18 12:56:24 -0300

    release: publish centered workout values

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v44';
+const CACHE = 'traco-v45';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
