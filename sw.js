33a367ae584812ac92384ce347d66a986be6ab58
caueccipriano <caueccipriano@gmail.com>
2026-09-18 12:13:49 -0300

    release: refresh Traço 2.4.0 PWA cache v41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v40';
+const CACHE = 'traco-v41';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
