35d500ef4ca169e34b5935a40b87ee3a604179d3
caueccipriano <caueccipriano@gmail.com>
2026-09-18 06:56:54 -0300

    chore: bump offline cache for active exercise switcher

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v19';
+const CACHE = 'traco-v20';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
