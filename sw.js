b3a99eb9789ee5e665fcd1a272c605d058db604f
caueccipriano <caueccipriano@gmail.com>
2026-09-18 08:28:39 -0300

    chore: refresh PWA cache for video hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v25';
+const CACHE = 'traco-v26';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
