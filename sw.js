7a3890dcb9a1f3fa91f094146f9009e0bd1d0dc9
caueccipriano <caueccipriano@gmail.com>
2026-09-18 13:04:57 -0300

    release: publish stacked workout units

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v46';
+const CACHE = 'traco-v47';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
