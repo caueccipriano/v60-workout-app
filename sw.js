b8eaca446d2c8eda03c9b13671e574bcea1ef7c1
caueccipriano <caueccipriano@gmail.com>
2026-09-18 12:37:57 -0300

    release: refresh cache for workout list rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v41';
+const CACHE = 'traco-v42';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
