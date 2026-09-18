0cdb3f17743f6b2cca9569bdcc4bf970a978ca66
caueccipriano <caueccipriano@gmail.com>
2026-09-18 10:49:38 -0300

    chore: refresh PWA cache for video fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v32';
+const CACHE = 'traco-v33';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
