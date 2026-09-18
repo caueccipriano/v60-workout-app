f5da809e7d53db646afe5e4fa68f40b7ad2c42e8
caueccipriano <caueccipriano@gmail.com>
2026-09-17 23:06:45 -0300

    chore: refresh cache for Traço 2.2.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v12';
+const CACHE = 'traco-v13';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
