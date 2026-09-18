55433e384e02e4e1d55b0347de20a095628c8079
caueccipriano <caueccipriano@gmail.com>
2026-09-18 06:34:46 -0300

    chore: bump offline cache for pre-start organizer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v16';
+const CACHE = 'traco-v17';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
