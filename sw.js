0337c66beefc121f5cc6a2d50d5c294513e56cd7
caueccipriano <caueccipriano@gmail.com>
2026-09-18 07:43:39 -0300

    chore: rotate offline cache for 2.3.8 hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v22';
+const CACHE = 'traco-v23';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
