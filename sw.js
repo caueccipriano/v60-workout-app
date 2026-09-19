58ffe0d7704be6443f3f970ac1b405ae3c029e5e
caueccipriano <caueccipriano@gmail.com>
2026-09-19 14:00:40 -0300

    chore: renovar cache final do Traço Coach

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v55';
+const CACHE = 'traco-v56';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
