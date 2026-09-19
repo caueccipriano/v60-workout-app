20742768f457be19367c37b111619a392f2576d0
caueccipriano <caueccipriano@gmail.com>
2026-09-19 16:43:41 -0300

    chore: renovar cache das fotos padronizadas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v59';
+const CACHE = 'traco-v60';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
