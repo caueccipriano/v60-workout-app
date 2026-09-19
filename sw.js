7b3c11c62040538041c2066c59f0c86401ca4c75
caueccipriano <caueccipriano@gmail.com>
2026-09-19 13:26:31 -0300

    chore: renovar cache PWA dorsal V

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v52';
+const CACHE = 'traco-v53';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
