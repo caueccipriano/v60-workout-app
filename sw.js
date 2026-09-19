1db73661d500668af128ca5e3bc7127335750fb8
caueccipriano <caueccipriano@gmail.com>
2026-09-19 11:57:36 -0300

    chore: renovar cache PWA para treino atualizado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v50';
+const CACHE = 'traco-v51';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
