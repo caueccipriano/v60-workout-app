0937a8aff8cc0755d3552d7beee17ca26aeb76ad
caueccipriano <caueccipriano@gmail.com>
2026-09-20 14:35:06 -0300

    chore: alinhar runtime ao build 283

--- traco-runtime.js
+++ traco-runtime.js
@@ -6,8 +6,8 @@
   'use strict';
 
   const VERSION='1.0.0';
-  const BUILD='performance-home-visible-2.4.1';
-  const BUILD_NUMBER='282';
+  const BUILD='runtime-stable-home-settings-2.4.1';
+  const BUILD_NUMBER='283';
   let reloading=false;
 
   const qs=s=>document.querySelector(s);
