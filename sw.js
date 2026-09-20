bc6ec6f972e5be30f17566bf9aa342aba4380712
caueccipriano <caueccipriano@gmail.com>
2026-09-20 16:04:45 -0300

    chore: renovar cache 291

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v75';
+const CACHE = 'traco-v76';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
