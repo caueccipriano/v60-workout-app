d115d0863d55c491806e1c0814a5bf8b27386906
caueccipriano <caueccipriano@gmail.com>
2026-09-20 12:22:11 -0300

    chore: renovar cache da variação com halteres

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v61';
+const CACHE = 'traco-v62';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
