5a109f0cea3d143717f10952406e5295fd6ee369
caueccipriano <caueccipriano@gmail.com>
2026-09-20 15:34:15 -0300

    chore: alinhar runtime ao UX build 289

--- traco-runtime.js
+++ traco-runtime.js
@@ -6,8 +6,8 @@
   'use strict';
 
   const VERSION='1.0.0';
-  const BUILD='pwa-icons-v2-red-black-2.4.1';
-  const BUILD_NUMBER='288';
+  const BUILD='ux-polish-information-architecture-2.4.1';
+  const BUILD_NUMBER='289';
   let reloading=false;
 
   const qs=s=>document.querySelector(s);
