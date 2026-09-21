473816839d182e26a0ecb933110022cdf3d55475
caueccipriano <caueccipriano@gmail.com>
2026-09-21 16:43:44 -0300

    Bump offline cache for polished release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v82';
+const CACHE = 'traco-v83';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
