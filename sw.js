2af0d9e4db5c142a73a973d4ce2e5371c350709d
caueccipriano <caueccipriano@gmail.com>
2026-09-21 20:50:01 -0300

    Release Shape Lab QA v346: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v91';
+const CACHE = 'traco-v92';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
