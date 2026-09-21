fa74cddca295a2c388a7c85f17d32c2553f4b429
caueccipriano <caueccipriano@gmail.com>
2026-09-21 20:17:18 -0300

    Refresh PWA cache for v340

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v87';
+const CACHE = 'traco-v88';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
