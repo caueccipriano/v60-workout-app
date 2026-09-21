ad417d57ccb28f4e5fa6f6a5010d6b78d849de4d
caueccipriano <caueccipriano@gmail.com>
2026-09-21 20:30:28 -0300

    Release route cleanup v343: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v90';
+const CACHE = 'traco-v91';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
