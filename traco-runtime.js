462fa6e571514987579990128b8ffc87dc72d768
caueccipriano <caueccipriano@gmail.com>
2026-09-21 20:17:22 -0300

    Align runtime with v340

--- traco-runtime.js
+++ traco-runtime.js
@@ -5,9 +5,9 @@
 (function(){
   'use strict';
 
-  const VERSION='1.6.0';
-  const BUILD='contrast-release-v338';
-  const BUILD_NUMBER='338';
+  const VERSION='1.6.1';
+  const BUILD='workout-integrity-v340';
+  const BUILD_NUMBER='340';
   let reloading=false;
 
   const qs=s=>document.querySelector(s);
