431dcf161d002396b298c152547f591ec48f742d
caueccipriano <caueccipriano@gmail.com>
2026-09-21 20:07:03 -0300

    Force fresh PWA cache v87

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v86';
+const CACHE = 'traco-v87';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
