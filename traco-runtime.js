28abac3c863a547b824e1d72e3b3a73e8ad700ec
caueccipriano <caueccipriano@gmail.com>
2026-09-22 08:50:11 -0300

    Release v366: traco-runtime.js

--- traco-runtime.js
+++ traco-runtime.js
@@ -5,9 +5,9 @@
 (function(){
   'use strict';
 
-  const VERSION='1.6.8';
-  const BUILD='sequence-fix-v365';
-  const BUILD_NUMBER='365';
+  const VERSION='1.6.9';
+  const BUILD='sequence-anchor-v366';
+  const BUILD_NUMBER='366';
   let reloading=false;
 
   const qs=s=>document.querySelector(s);
