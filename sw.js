9adff226b4063397408b1f4f2cdabfb0ae355ebf
caueccipriano <caueccipriano@gmail.com>
2026-09-22 08:44:52 -0300

    Release v365 clean cache: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v97';
+const CACHE = 'traco-v98';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
