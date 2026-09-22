9eb3157c08846ce3a285baba1a6eb1d4831915eb
caueccipriano <caueccipriano@gmail.com>
2026-09-21 21:11:28 -0300

    Release Traço design system v357: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v93';
+const CACHE = 'traco-v94';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
