04ed44c154c305786af9dd41cb2bc9add774dfd5
caueccipriano <caueccipriano@gmail.com>
2026-09-21 21:42:37 -0300

    Release v361: cache-bust sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v94';
+const CACHE = 'traco-v95';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
