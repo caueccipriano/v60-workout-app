fef7198e774fa928e96b654dbd59d54f0968310f
caueccipriano <caueccipriano@gmail.com>
2026-09-24 21:25:46 -0300

    Traço: alinhar treino ao inventário confirmado da academia (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'traco-v100';
+const CACHE = 'traco-v101';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './styles.css','./exercise-guides.css','./exercise-guides-v4.css','./exercise-videos.css',
